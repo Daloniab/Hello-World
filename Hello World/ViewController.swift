9b1f539ed1af46658e4740584cc742372d8c583f
Daloniab <daloniab@aol.com>
2016-05-26 16:10:52 -0500

    initial commit
    this is my first commit ever!

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+
+    @IBAction func ButtonPressed(sender: AnyObject) {
+      
+        helloWorldLabel.hidden = !helloWorldLabel.hidden
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
